f2c98dedb19ab875e66cfe98f3c98eb7227ab5fa
MinexAutomation <minex.automation@gmail.com>
2020-05-15 23:37:12 -0700

    Added R5T.D0012.Base library.

---
+++ source/R5T.D0012.Base/Code/Services/Definitions/IProcessStartTimeUtcDirectoryNameProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Threading.Tasks;
+
+
+namespace R5T.D0012
+{
+    public interface IProcessStartTimeUtcDirectoryNameProvider
+    {
+        Task<string> GetProcessStartTimeUtcDirectoryName();
+    }
+}
