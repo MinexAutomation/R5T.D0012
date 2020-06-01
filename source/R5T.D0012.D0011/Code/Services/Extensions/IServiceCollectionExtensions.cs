9dfbd9e52e1cb9d773ff7352a986c087a5d78bf0
MinexAutomation <minex.automation@gmail.com>
2020-06-01 11:25:36 -0700

    Added IProcessStartTimeUtcProvider and ITimestampDirectoryNameProvider service dependencies.

--- source/R5T.D0012.D0011/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.D0012.D0011/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -4,6 +4,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Dacia;
 
+using R5T.D0008;
+using R5T.D0011;
+
 
 namespace R5T.D0012.D0011
 {
@@ -12,9 +15,15 @@ namespace R5T.D0012.D0011
         /// <summary>
         /// Adds the <see cref="ProcessStartTimeUtcDirectoryNameProvider"/> implementation of <see cref="IProcessStartTimeUtcDirectoryNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
         /// </summary>
-        public static IServiceCollection AddD0011ProcessStartTimeUtcDirectoryNameProvider(this IServiceCollection services)
+        public static IServiceCollection AddD0011ProcessStartTimeUtcDirectoryNameProvider(this IServiceCollection services,
+            IServiceAction<IProcessStartTimeUtcProvider> processStartTimeUtcProvider,
+            IServiceAction<ITimestampUtcDirectoryNameProvider> timestampUtcDirectoryNameProvider)
         {
-            services.AddSingleton<IProcessStartTimeUtcDirectoryNameProvider, ProcessStartTimeUtcDirectoryNameProvider>();
+            services
+                .AddSingleton<IProcessStartTimeUtcDirectoryNameProvider, ProcessStartTimeUtcDirectoryNameProvider>()
+                .Run(processStartTimeUtcProvider)
+                .Run(timestampUtcDirectoryNameProvider)
+                ;
 
             return services;
         }
@@ -22,9 +31,13 @@ namespace R5T.D0012.D0011
         /// <summary>
         /// Adds the <see cref="ProcessStartTimeUtcDirectoryNameProvider"/> implementation of <see cref="IProcessStartTimeUtcDirectoryNameProvider"/> as a <see cref="ServiceLifetime.Singleton"/>.
         /// </summary>
-        public static ServiceAction<IProcessStartTimeUtcDirectoryNameProvider> AddD0011ProcessStartTimeUtcDirectoryNameProviderAction(this IServiceCollection services)
+        public static ServiceAction<IProcessStartTimeUtcDirectoryNameProvider> AddD0011ProcessStartTimeUtcDirectoryNameProviderAction(this IServiceCollection services,
+            IServiceAction<IProcessStartTimeUtcProvider> processStartTimeUtcProvider,
+            IServiceAction<ITimestampUtcDirectoryNameProvider> timestampUtcDirectoryNameProvider)
         {
-            var serviceAction = ServiceAction<IProcessStartTimeUtcDirectoryNameProvider>.New(() => services.AddD0011ProcessStartTimeUtcDirectoryNameProvider());
+            var serviceAction = ServiceAction<IProcessStartTimeUtcDirectoryNameProvider>.New(() => services.AddD0011ProcessStartTimeUtcDirectoryNameProvider(
+                processStartTimeUtcProvider,
+                timestampUtcDirectoryNameProvider));
             return serviceAction;
         }
     }
