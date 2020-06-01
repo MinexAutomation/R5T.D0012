7cce1ceadf4f669e3408f6265a4c90c4a47a758d
MinexAutomation <minex.automation@gmail.com>
2020-06-01 11:26:08 -0700

    Added IProcessStartTimeUtcProvider and ITimestampDirectoryNameProvider service dependencies.

--- source/R5T.D0012.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
+++ source/R5T.D0012.Standard/Code/Services/Extensions/IServiceCollectionExtensions.cs
@@ -4,6 +4,10 @@ using Microsoft.Extensions.DependencyInjection;
 
 using R5T.Dacia;
 
+using R5T.D0008;
+using R5T.D0008.Standard;
+using R5T.D0011;
+using R5T.D0011.Standard;
 using R5T.D0012.D0011;
 
 
@@ -14,9 +18,13 @@ namespace R5T.D0012.Standard
         /// <summary>
         /// Adds the <see cref="IProcessStartTimeUtcDirectoryNameProvider"/> service.
         /// </summary>
-        public static IServiceCollection AddProcessStartTimeUtcDirectoryNameProvider(this IServiceCollection services)
+        public static IServiceCollection AddProcessStartTimeUtcDirectoryNameProvider(this IServiceCollection services,
+            IServiceAction<IProcessStartTimeUtcProvider> processStartTimeUtcProvider,
+            IServiceAction<ITimestampUtcDirectoryNameProvider> timestampUtcDirectoryNameProvider)
         {
-            services.AddD0011ProcessStartTimeUtcDirectoryNameProvider();
+            services.AddD0011ProcessStartTimeUtcDirectoryNameProvider(
+                processStartTimeUtcProvider,
+                timestampUtcDirectoryNameProvider);
 
             return services;
         }
@@ -24,10 +32,38 @@ namespace R5T.D0012.Standard
         /// <summary>
         /// Adds the <see cref="IProcessStartTimeUtcDirectoryNameProvider"/> service.
         /// </summary>
-        public static ServiceAction<IProcessStartTimeUtcDirectoryNameProvider> AddProcessStartTimeUtcDirectoryNameProviderAction(this IServiceCollection services)
+        public static IServiceCollection AddProcessStartTimeUtcDirectoryNameProvider(this IServiceCollection services)
         {
-            var serviceAction = ServiceAction<IProcessStartTimeUtcDirectoryNameProvider>.New(() => services.AddProcessStartTimeUtcDirectoryNameProvider());
+            services.AddProcessStartTimeUtcDirectoryNameProvider(
+                services.AddProcessStartTimeUtcProviderAction(),
+                services.AddTimestampUtcDirectoryNameProviderAction());
+
+            return services;
+        }
+
+        public static IServiceAction<IProcessStartTimeUtcDirectoryNameProvider> AddProcessStartTimeUtcDirectoryNameProviderAction(this IServiceCollection services,
+            IServiceAction<IProcessStartTimeUtcProvider> processStartTimeUtcProvider,
+            IServiceAction<ITimestampUtcDirectoryNameProvider> timestampUtcDirectoryNameProvider)
+        {
+            var serviceAction = ServiceAction<IProcessStartTimeUtcDirectoryNameProvider>.New(() => services.AddProcessStartTimeUtcDirectoryNameProvider(
+                processStartTimeUtcProvider,
+                timestampUtcDirectoryNameProvider));
             return serviceAction;
+        }
+
+        /// <summary>
+        /// Adds the <see cref="IProcessStartTimeUtcDirectoryNameProvider"/> service.
+        /// </summary>
+        public static (
+            IServiceAction<IProcessStartTimeUtcDirectoryNameProvider> processStartTimeUtcDirectoryNameProviderAction,
+            IServiceAction<IProcessStartTimeUtcProvider> processStartTimeUtcProviderAction,
+            IServiceAction<ITimestampUtcDirectoryNameProvider> timestampUtcDirectoryNameProviderAction) AddProcessStartTimeUtcDirectoryNameProviderAction(this IServiceCollection services)
+        {
+            var output = (
+                processStartTimeUtcDirectoryNameProviderAction: ServiceAction<IProcessStartTimeUtcDirectoryNameProvider>.New(() => services.AddProcessStartTimeUtcDirectoryNameProvider()),
+                processStartTimeUtcProviderAction: ServiceAction<IProcessStartTimeUtcProvider>.AlreadyAdded,
+                timestampUtcDirectoryNameProviderAction: ServiceAction<ITimestampUtcDirectoryNameProvider>.AlreadyAdded);
+            return output;
         }
     }
 }
